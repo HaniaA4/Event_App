6cb7049469b321ba1133b1516c2d7c2f1281c470
danielasofia67 <dani___@sapo.pt>
2026-06-11 21:22:29 +0100

    Add status, max_participants, and map_link to Event model

--- models.py
+++ models.py
@@ -22,6 +22,9 @@ class Event(BaseModel):
     description = TextField(null=True)
     date = DateTimeField()
     location = CharField()
+    status = CharField(default="open")
+    max_participants = IntegerField(null=True)
+    map_link = Charfield(null=True)
     category = ForeignKeyField(Category, backref="events")
     organizer = ForeignKeyField(User, backref="events")
     # Number of participants is not stored here, it will be counted from registrations: event.registrations.count().
@@ -31,9 +34,14 @@ class Registration(BaseModel):
     user = ForeignKeyField(User, backref="registrations")
     event = ForeignKeyField(Event, backref="registrations")
 
+class Comment(BaseModel):
+    text = TextField()
+    event = ForeignKeyField(Event, backref="comments")
+    author = ForeignKeyField(User, backref="comments")
+
 
 def initialize_database():
     db.connect()
-    db.create_tables([User, Category, Event, Registration])
+    db.create_tables([User, Category, Event, Registration, Comment])
     db.close()
   
